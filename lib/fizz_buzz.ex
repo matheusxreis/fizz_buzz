545170f540b813574bb1feb30140b43c66550bd0
Matheus Reis <matheus.reis081@gmail.com>
2022-09-08 17:42:40 -0300

    using defp to let public functions private

--- lib/fizz_buzz.ex
+++ lib/fizz_buzz.ex
@@ -5,21 +5,23 @@ defmodule FizzBuzz do
     |> handle_file_read()
   end
 
-  def handle_file_read({:ok, result}) do
+  defp handle_file_read({:ok, result}) do
     result
     |> String.split(",")
     |> Enum.map(&convert_and_evaluate_number/1)
   end
-  def handle_file_read({:error, reason}), do: "Error reading to file: #{reason}"
-  def convert_and_evaluate_number(elem) do
+  defp handle_file_read({:error, reason}), do: "Error reading to file: #{reason}"
+  @spec convert_and_evaluate_number(binary) :: any
+  defp convert_and_evaluate_number(elem) do
     elem
     |> String.to_integer()
     |> evaluate_number()
   end
-  def evaluate_number(number) when rem(number, 3) === 0 and rem(number, 5) == 0, do: "fizzbuzz"
-  def evaluate_number(number) when rem(number, 3) == 0, do: "fizz"
-  def evaluate_number(number) when rem(number, 5) == 0, do: "buzz"
-  def evaluate_number(number), do: number
+  @spec evaluate_number(any) :: any
+  defp evaluate_number(number) when rem(number, 3) === 0 and rem(number, 5) == 0, do: "fizzbuzz"
+  defp evaluate_number(number) when rem(number, 3) == 0, do: "fizz"
+  defp evaluate_number(number) when rem(number, 5) == 0, do: "buzz"
+  defp evaluate_number(number), do: number
 
 
 
