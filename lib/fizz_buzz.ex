5e32a776ec953f0745f661654f966a0f8cfe8cb0
Matheus Reis <matheus.reis081@gmail.com>
2022-09-08 17:32:02 -0300

    feat: created function convert_and_evalute_number

--- lib/fizz_buzz.ex
+++ lib/fizz_buzz.ex
@@ -8,7 +8,15 @@ defmodule FizzBuzz do
   def handle_file_read({:ok, result}) do
     result
     |> String.split(",")
-    |> Enum.map(&String.to_integer/1)
+    |> Enum.map(&convert_and_evaluate_number/1)
   end
   def handle_file_read({:error, reason}), do: "Error reading to file: #{reason}"
+  def convert_and_evaluate_number(elem) do
+    elem
+    |> String.to_integer()
+    |> evaluate_number()
+  end
+  def evaluate_number(number) do
+
+  end
 end
