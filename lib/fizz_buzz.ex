d95ad641daea1ec862cea97a65f3a244b5522621
Matheus Reis <matheus.reis081@gmail.com>
2022-09-08 14:39:22 -0300

    feat: doing pattern matching directly in function argument

--- lib/fizz_buzz.ex
+++ lib/fizz_buzz.ex
@@ -1,8 +1,8 @@
 defmodule FizzBuzz do
   def build(file_name) do
-    case File.read(file_name) do
-      {:ok, result} -> result
-      {:error, reason} -> reason
-    end
+    handle_file_read(File.read(file_name))
   end
+
+  def handle_file_read({:ok, result}), do: result
+  def handle_file_read({:error, reason}), do: reason
 end
